f56ea845596509027b778292f37b0633b8a5979e
Miguel Cabrera <miguelcabsan@gmail.com>
2020-05-17 01:03:21 +0100

    finish(lesson 16)

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,12 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
